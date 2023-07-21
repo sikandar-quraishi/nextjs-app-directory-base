fd43709560a6403b358b002de7af7d4db57dca92
Sikandar Quraishi <sikandar.quraishi@xetech.com>
2023-07-21 18:03:05 +0530

    directory change and modification

--- app/auth/register/page.js
+++ app/auth/register/page.js
@@ -1,9 +1,61 @@
-import React from 'react'
+// components/RegisterForm.js
+'use client'
+import { useState } from 'react';
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
 
-export default Register
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("formData", formData)
+
+    // try {
+    //   const response = await fetch('/api/register', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify(formData),
+    //   });
+
+    //   const data = await response.json();
+    //   console.log(data); // Handle the response accordingly
+    // } catch (error) {
+    //   console.error('Error:', error);
+    // }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className='registerFrom'>
+      <label>
+        Username:
+        <input type="text" name="username" value={formData.username} onChange={handleChange} />
+      </label>
+      <label>
+        Email:
+        <input type="email" name="email" value={formData.email} onChange={handleChange} />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+      </label>
+      <button type="submit">Register</button>
+    </form>
+  );
+};
+
+export default Register;
